840c32a4cbda159b93069f96b049c1b3aa13bfab
clxrity <97744702+clxrityy@users.noreply.github.com>
2022-02-02 22:11:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-<head>
+
     function myFunction(elmnt, clr) {
         elmnt.style.color = clr;
       }
-</head>
-<body>
-<h1 onmousedown="myFunction(this,'#77c7af')" onmouseup="myFunction(this,'#0d9c9b')"><b>things &amp; stuff </b>
-    </h1>
-    <p1 onmousedown="myFunction(this,'#0d9c9b')" onmouseup="myFunction(this,'#77c7af')">
-      <a href="https://twitter.com/yourclxrity" title="@yourclxrity"><img src="https://imgur.com/ByWIoDe.png" width="15" height="13" aria-hidden="true"></a> Twitter
-      <br>
-      <a href="https://instagram.com/mjxnglin" title="@mjxnglin"><img src="https://imgur.com/XLnUh3i.png" width="15" height="13" aria-hidden="true"></a> Instagram
-      <br>
-      <a href="https://soundcloud.com/clxrityy" title="@clxrityy"><img src="https://imgur.com/ZdvfWsi.png" width="15" height="13" aria-hidden="true"></a> Soundcloud
-      <br>
-      <a href="https://github.com/clxrityy" title="@clxrityy"><img src="https://imgur.com/1xqE0Hb.png" width="15" height="13" aria-hidden="true"></a> GitHub
-      <a href="https://open.spotify.com/user/mjanglin" title="@mjanglin">
-        <br>
-        <img src="https://imgur.com/dGRMUW1.png" width="15" height="13" aria-hidden="true"></a> Spotify
-<br>
-<a href="https://steamcommunity.com/id/clxrity/" title="@clxrity"><img src="https://imgur.com/rM25ANu.png" width="15" height="13" aria-hidden="true"></a> Steam
-      <br>
-      <br>
-      <img src="https://imgur.com/Lf8uBi8.png" width="15" height="13" aria-hidden="true"> Discord: <b>clarity#0001</b>
-    </p1>
-</body>
