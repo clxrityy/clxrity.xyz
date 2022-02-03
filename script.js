25b61a11dded21f1085492a1d0e247de465de108
clxrity <97744702+clxrityy@users.noreply.github.com>
2022-02-02 21:50:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
       function myFunction(elmnt, clr) {
         elmnt.style.color = clr;
       }
+<h1 onmousedown="myFunction(this,'#77c7af')" onmouseup="myFunction(this,'#0d9c9b')"><b>things &amp; stuff </b>
+    </h1>
