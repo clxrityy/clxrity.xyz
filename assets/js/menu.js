812a70fd0f82048aea9d636a58bb387187e2cb89
clxrity <clxrityxyz@gmail.com>
2022-07-25 14:58:42 -0400

    missing semi lol

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,4 +4,4 @@ const menuLinks = document.querySelector('.navbar__menu')
 menu.addEventListener('click', function (){
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
-})
+});
