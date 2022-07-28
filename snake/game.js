a19f8a16ffca98f8327c26dfdf203d92ddf40ca2
clxrity <clxrityxyz@gmail.com>
2022-07-28 15:11:11 -0400

    add snake

---
+++ snake/game.js
@@ -0,0 +1,44 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from 'snake/snake.js'
+import { update as updateFood, draw as drawFood } from 'snake/food.js'
+import { outsideGrid } from 'snake/grid.js'
+
+let lastRenderTime = 0
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
+
+function main(currentTime) {
+    if (gameOver) {
+        if (confirm('You lost. Press ok to restart')) {
+            window.location = '/'
+        }
+        return
+    }
+
+    window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+    updateFood()
+    checkDeath()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
+}
