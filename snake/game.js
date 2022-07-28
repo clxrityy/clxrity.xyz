caa1751f2d1b725657df402ebb4d2797233ed391
clxrity <clxrityxyz@gmail.com>
2022-07-28 15:20:44 -0400

    fix game restart

--- snake/game.js
+++ snake/game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
     if (gameOver) {
         if (confirm('You lost. Press ok to restart')) {
-            window.location = 'snake/snake.html'
+            window.location = './snake.html'
         }
         return
     }
