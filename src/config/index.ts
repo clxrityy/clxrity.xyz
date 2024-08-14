5e5b2a76af2c9849748bbb11e9a945e4ce47c514
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-08-13 21:44:14 -0400

    fix again!

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,12 @@
-import { AudioItem } from "@/archive/Preview";
+// import { AudioItem } from "@/archive/Preview";
 
-export const audioItems: AudioItem[] = [
-    {
-        title: "Dreamy guitar loop",
-        key: "E♭ Major",
-        bpm: 118,
-        src: "./assets/audio/previews/dreamy-guitar-loop.mp3"
-    }
-]
+// export const audioItems: AudioItem[] = [
+//     {
+//         title: "Dreamy guitar loop",
+//         key: "E♭ Major",
+//         bpm: 118,
+//         src: "./assets/audio/previews/dreamy-guitar-loop.mp3"
+//     }
+// ]
 
 export const maxBytes = 25000000;
