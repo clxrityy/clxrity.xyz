2b2479fd312956a7cda6b18e98d22ad45e682d6b
clxrityy <97744702+clxrityy@users.noreply.github.com>
2024-09-01 15:17:46 -0400

    update profile

--- src/app/(routes)/(auth)/profile/[id]/layout.tsx
+++ src/app/(routes)/(auth)/profile/[id]/layout.tsx
@@ -1,4 +1,6 @@
+import { getUploadsByUser } from "@/app/(actions)/uploads";
 import getUser from "@/app/(actions)/users";
+import AudioItem from "@/components/audio/AudioItem";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 
@@ -31,12 +33,21 @@ export default async function Layout({ children, params }: { children: React.Rea
         return redirect("/");
     }
 
+    const userUploads = await getUploadsByUser(params.id);
 
     return (
         <div className="w-full h-full">
-            <div className="flex flex-col gap-5 items-center justify-center">
-                
+            <div className="flex flex-col gap-10 items-center justify-center">
                 {children}
+                <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-center justify-center">
+                    {
+                        userUploads.length && userUploads.map((upload, idx) => (
+                            <div className="bg-black/45 rounded-xl px-5 py-2 w-full shadow-xl drop-shadow-2xl" key={idx}>
+                                <AudioItem key={idx} audio={upload} />
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     );
