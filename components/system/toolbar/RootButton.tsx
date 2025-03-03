c75ed1ba6aa8be960c986c27bd93927188b1c12a
clxrityy <clxrityxyz@gmail.com>
2025-03-03 16:53:35 -0500

    tool bar styles

--- components/system/toolbar/RootButton.tsx
+++ components/system/toolbar/RootButton.tsx
@@ -3,8 +3,9 @@ import { ImageComponent } from "../../ui/Image";
 export const RootButton = () => {
   return (
     <button
+      title="Root"
       type="button"
-      className={`h-15 w-18 shadow-inner flex rounded-4xl items-center justify-center cursor-pointer transition-all duration-100 ease-linear bg-gradient-to-r from-[var(--background-start)]/15 to-[var(--background-start)]/5 hover:bg-[var(--background-end)]/[2.5%] focus:bg-transparent focus:scale-[92.5%] ring-offset-1 ring-offset-zinc-900/15 focus:rounded-3xl focus:contrast-150 focus:drop-shadow-lg place-items-center place-content-center`}
+      className={`h-15 w-18 shadow-inner flex rounded-4xl items-center justify-center transition-all duration-100 ease-linear bg-gradient-to-r from-[var(--background-start)]/15 to-[var(--background-start)]/5 hover:bg-[var(--background-end)]/[2.5%] focus:bg-transparent focus:scale-[92.5%] ring-offset-1 ring-offset-zinc-900/15 focus:rounded-3xl focus:contrast-200 focus:drop-shadow-lg place-items-center place-content-center`}
     >
       <ImageComponent
         image={{
--- components/system/toolbar/Toolbar.tsx
+++ components/system/toolbar/Toolbar.tsx
@@ -1,9 +1,10 @@
+import "@/styles/toolbar/index.css";
 import { Clock } from "./Clock";
 import { RootButton } from "./RootButton";
 
 export const Toolbar = () => {
   return (
-    <nav className="top-0 fixed bg-gradient-to-r from-[var(--background-start)] to-[var(--background-end)] w-screen h-16 drop-shadow-md shadow-inner">
+    <nav role="nav">
       {/**
        * - Root button (logo)
        *
@@ -19,7 +20,7 @@ export const Toolbar = () => {
           {/* LEFT container */}
           <li className="w-4/5 flex items-center justify-start gap-5">
             <ul className="flex items-center justify-start gap-5 w-full lg:w-3/4 xl:w-1/2 2xl:w-1/3">
-              <li className="w-[15%] lg:w-[20%] 2xl:w-[30%] bg-blue-500 h-10">
+              <li className="w-[15%] lg:w-[20%] 2xl:w-[30%] bg-blue-500 h-12">
                 {/*
                   Clock
                     /
@@ -27,7 +28,7 @@ export const Toolbar = () => {
                 */}
                 <Clock />
               </li>
-              <li className="w-[25%] lg:w-[30%] 2xl:w-[40%] bg-red-500 h-10">
+              <li className="w-[25%] lg:w-[30%] 2xl:w-[40%] bg-red-500 h-12 flex items-center justify-center gap-2 lg:gap-4 xl:gap-6">
                 {/* Tools */}
               </li>
             </ul>
