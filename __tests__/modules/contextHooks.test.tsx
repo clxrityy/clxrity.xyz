5db51b68fc2d88a7500bb6a36621cb681853edd5
clxrityy <clxrityxyz@gmail.com>
2025-03-04 13:52:41 -0500

    rearrange

--- __tests__/modules/contextHooks.test.tsx
+++ __tests__/modules/contextHooks.test.tsx
@@ -2,6 +2,14 @@ import { useSessionContextState } from "@/hooks/useSessionContextState";
 import { processDirectory } from "@/util/processDirectory";
 import { contextFactory } from "@/util/contextFactory";
 
+// CONTEXT FACTORY
+test("contextFactory returns a Provider & Consumer", () => {
+  const { Consumer, Provider } = contextFactory({}, () => ({}));
+
+  expect(Consumer).toBeDefined();
+  expect(Provider).toBeDefined();
+});
+
 // SESSION CONTEXT HOOKS
 test("session data & settings are defined", () => {
   const { data, settings } = useSessionContextState();
@@ -14,12 +22,4 @@ test("session data & settings are defined", () => {
 // PROCESS DIRECTORY
 test("processDirectory is defined", () => {
   expect(processDirectory).toBeDefined();
-});
-
-// CONTEXT FACTORY
-test("contextFactory returns a Provider & Consumer", () => {
-  const { Consumer, Provider } = contextFactory({}, () => ({}));
-
-  expect(Consumer).toBeDefined();
-  expect(Provider).toBeDefined();
 });
