9432ad5bb73bc26cad928c43e99d5d06d65a3269
clxrityy <clxrityxyz@gmail.com>
2025-03-04 14:14:11 -0500

    refactor file system context

--- hooks/useFileSystemContextState.tsx
+++ hooks/useFileSystemContextState.tsx
@@ -1,25 +1,22 @@
 "use client";
 import { useEffect, useState } from "react";
-import * as BrowserFS from "browserfs";
+import { configure, BFSRequire, type FileSystemConfiguration } from "browserfs";
 import { FSModule } from "browserfs/dist/node/core/FS";
 import { FileSystemContextState } from "@/context/fileSystem";
+
+export const fileSystemConfig: FileSystemConfiguration = {
+  fs: "IndexedDB",
+  options: {},
+};
 
 export const useFileSystemContextState = (): FileSystemContextState => {
   const [fs, setFs] = useState<FSModule | null>(null);
 
   useEffect(() => {
-    BrowserFS.install(window);
-
-    BrowserFS.configure(
-      {
-        fs: "IndexedDB",
-        options: {},
-      },
-      () => {
-        setFs(BrowserFS.BFSRequire("fs"));
-      },
-    );
-  }, []);
+    if (!fs) {
+      configure(fileSystemConfig, () => setFs(BFSRequire("fs")));
+    }
+  }, [fs]);
 
   return { fs };
 };
