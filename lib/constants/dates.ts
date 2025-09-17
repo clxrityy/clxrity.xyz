6c244fea1b99f0cea13d6de28ac8ce3527325b0b
clxrityy <clxrityxyz@gmail.com>
2025-09-17 11:25:03 -0400

    add constant

--- lib/constants/dates.ts
+++ lib/constants/dates.ts
@@ -2,3 +2,5 @@ export const MONTH_NAMES = [
     'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
 ] as const;
 export type MonthName = typeof MONTH_NAMES[number];
+
+export const MONTH_LENGTHS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
