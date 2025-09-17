88f003258bf6e772de5dc5da9e436a83b17f6462
clxrityy <clxrityxyz@gmail.com>
2025-09-17 14:11:38 -0400

    remove unnecessary origin

--- next.config.mjs
+++ next.config.mjs
@@ -30,7 +30,7 @@ const nextConfig = {
       },
     ];
   },
-  allowedDevOrigins: [process.env.CF_TUNNEL_URL],
+  // allowedDevOrigins: [process.env.CF_TUNNEL_URL]
 };
 
 export default nextConfig;
