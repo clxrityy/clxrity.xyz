530d4342647c6b4cc497ada7c72426fcc28fae0d
clxrityy <clxrityxyz@gmail.com>
2025-09-19 05:12:46 -0400

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,44 @@
+import type { MetadataRoute } from "next";
+
+
+export async function generateSitemap(): Promise<MetadataRoute.Sitemap> {
+    return [
+        {
+            url: "https://clxrity.xyz",
+            lastModified: new Date(),
+            priority: 1.0,
+            images: [
+                "https://clxrity.xyz/android-chrome-192x192.png"
+            ]
+        },
+        {
+            url: "https://clxrity.xyz/wav",
+            lastModified: new Date(),
+            priority: 0.5,
+            images: [
+                "https://wav.clxrity.xyz/android-chrome-192x192.png"
+            ]
+        },
+        {
+            url: "https://clxrity.xyz/mc",
+            lastModified: new Date(),
+            priority: 0.5,
+        },
+        {
+            url: "https://clxrity.xyz/os",
+            lastModified: new Date(),
+            priority: 0.5,
+            images: [
+                "https://os.clxrity.xyz/android-chrome-192x192.png"
+            ]
+        },
+        {
+            url: "https://clxrity.xyz/hbd",
+            lastModified: new Date(),
+            priority: 0.5,
+            images: [
+                "https://hbd.clxrity.xyz/android-chrome-192x192.png"
+            ]
+        }
+    ]
+}
