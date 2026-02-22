2769d63b67709d5cababe7425b7b37ab2b28f8bb
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-02-22 13:03:11 -0800

    feat(footer): add footer component with copyright and links

--- components/Desktop.tsx
+++ components/Desktop.tsx
@@ -2,10 +2,11 @@ import { desktopApps } from "../utils/desktopApps";
 import { Suspense } from "react";
 import { Skeleton } from "./Skeleton";
 import { AppButton } from "./AppButton";
+import { Footer } from "./Footer";
 
 export function Desktop() {
     return (
-        <div className="retro-desktop-bg min-h-screen w-full flex flex-wrap items-start p-8 relative">
+        <div className="retro-desktop-bg min-h-screen w-full flex flex-wrap items-start p-8 relative flex-col">
             <div className="desktop-icons flex flex-wrap">
                 {desktopApps.map((app, idx) => (
                     <Suspense key={`suspense-${app.name}-${idx}`} fallback={<Skeleton width="64px" height="80px" borderRadius="8px" className="m-4" />}>
@@ -13,6 +14,7 @@ export function Desktop() {
                     </Suspense>
                 ))}
             </div>
+						<Footer />
         </div>
     );
 }
