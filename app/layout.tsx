5c49e4812dc8284c96ae46ed5992f7b678e04bf9
clxrityy <97744702+clxrityy@users.noreply.github.com>
2026-02-22 12:43:36 -0800

    enhance metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
                 url: "https://clxrity.xyz/favicon.ico",
                 alt: "clxrity.xyz",
             }
-        ]
+        ],
     },
     twitter: {
         card: "summary",
@@ -33,6 +33,34 @@ export const metadata: Metadata = {
             { rel: "android-chrome-512x512", url: "https://clxrity.xyz/android-chrome-512x512.png" },
         ],
     },
+		authors: [
+			{
+				name: "MJ Anglin",
+				url: "https://mjanglin.com",
+			},
+			{
+				name: "clxrity",
+				url: "https://github.com/clxrityy",
+			}
+		],
+		creator: "MJ Anglin",
+		publisher: "clxrity",
+		metadataBase: new URL("https://clxrity.xyz"),
+		manifest: "https://clxrity.xyz/site.webmanifest",
+		appleWebApp: {
+			statusBarStyle: "black-translucent",
+			capable: true,
+		},
+		description: "Hub for clxrity networks.",
+		keywords: ["clxrity", "mjanglin", "portfolio", "developer", "designer", "blog", "projects", "github", "apps", "software", "technology", "music", "art", "writing", "clxrity.xyz", "mjanglin.com", "os", "retro desktop", "web apps"],
+		applicationName: "clxrity.xyz",
+		abstract: "Hub for clxrity networks.",
+		robots: {
+			index: true,
+			follow: true,
+		},
+		category: "technology",
+		classification: "hub",
 };
 
 export default async function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
